cdb41d3dfd67f639209108fc96183f83d74ebd75
Ashwani Kumar Tiwari <tiwariashwani3012@gmail.com>
2021-08-17 10:07:32 +0530

    Header complete

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
+import Particles from "./components/Particle";
 import Navbar from "./components/Navbar";
+import Header from './components/Header';
 function App() {
   return (
-    <Navbar></Navbar>
+    <>
+    <Particles/>
+    <Navbar/>
+    <Header/>
+    </>
   );
 }
 
